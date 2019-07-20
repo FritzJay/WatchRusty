81a41acafd60bcd88eedadfe3bcd59302c92c573
Fritz Jay <jonathan.jay10@gmail.com>
2019-07-19 19:20:41 -0700

    Fixed wording because pressing any key to exit doesn't actually work!

--- Watch-Rusty.ps1
+++ Watch-Rusty.ps1
@@ -4,7 +4,7 @@ $LocalTimeZone = Get-Timezone
 $9pmRustysTime = [System.TimeZoneInfo]::ConvertTime($9pm, $EasternStandardTime, $LocalTimeZone)
 
 $Trigger = New-ScheduledTaskTrigger -Daily -At $9pmRustysTime
-$Argument = "-Command `"Write-Output('Welcome to WatchRusty! Open up Windows PowerShell and paste the following to remove this scheduled task: Unregister-ScheduledTask WatchRusty -Confirm:`$false'); Explorer 'https://www.twitch.tv/rusty_neal'; Read-Host -Prompt 'Press any key to exit...'`""
+$Argument = "-Command `"Write-Output('Welcome to WatchRusty! Open up Windows PowerShell and paste the following to remove this scheduled task: Unregister-ScheduledTask WatchRusty -Confirm:`$false'); Explorer 'https://www.twitch.tv/rusty_neal'; Read-Host -Prompt 'Press enter to exit...'`""
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Argument
 $Settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
 $ScheduledTask = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
